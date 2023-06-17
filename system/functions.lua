a170deb27ff199e5b0fa48d6d96e9175a7b90815
Brennen Murphy <brennenmurph@pm.me>
2023-06-16 20:09:18 -0400

    Added credits function

--- system/functions.lua
+++ system/functions.lua
@@ -2,6 +2,40 @@
 list = {data = {}, len = 0, icons = {}, picons = {} }
 filenames = { fn = {}, len = 0 }
 
+function credits()
+	local pos = 1
+	local y = 85
+	local reverse = false
+	local ft = font.load("resources/fonts/Neuton-Bold.ttf")
+	font.setdefault(ft)
+	while true do
+		draw.gradrect(1, 1, 959, 543, color.blue, color.green, __DIAGONAL)
+		buttons.read()
+		screen.print(472, 20, "Credits", 1, color.red)	
+		screen.print(472, 35, "----------", 1, color.red)
+		screen.print(460, y, "Acid_Snake", 1, color.black, color.orange)
+		screen.print(460, y+20, "Yoti", 1, color.black, color.orange)
+		screen.print(460, y+40, "Krazynez", 1, color.black, color.orange)
+		screen.print(430, y+60, "#Blame Cypress", 1, color.black, color.orange)
+		if buttons.released.r then
+			font.setdefault()
+			break
+		end
+		if y > 409 and reverse == true then
+			y -= 1 
+		elseif y > 409 and reverse == false then
+			reverse = true
+		elseif y < 86 and reverse == true then
+			reverse = false
+		elseif reverse == true and y > 85 then
+			y -= 1
+		elseif reverse == false then
+			y += 1
+		end
+		screen.flip()
+	end
+end
+
 function find_license()
 	local pos = 1
 	buttons.homepopup(1)
